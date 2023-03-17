cfc8099c16507e8aa46a332eb7035d764a4f4103
CamillePB <camille.pedro.bueno@hotmail.com>
2023-03-17 16:28:14 -0300

    Exercicios updated 17/03

---
+++ aula_04/src/aula_06/Exercicio01.java
@@ -0,0 +1,27 @@
+package aula_06;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+
+		int num, impar = 0, par = 0;
+
+		for (int cont = 1; cont <= 10; cont++) {
+			System.out.println("Digite o " + cont + "° número: ");
+			num = leia.nextInt();
+
+			if ((num % 2) == 0)
+				par++;
+			else if ((num % 2) != 0)
+				impar++;
+
+		}
+
+		System.out.println("Números pares: " + par);
+		System.out.println("Números impares: " + impar);
+	}
+}
