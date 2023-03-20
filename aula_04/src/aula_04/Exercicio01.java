454088a9de5cbe3c275b2dcde66986418de07996
CamillePB <camille.pedro.bueno@hotmail.com>
2023-03-20 15:20:53 -0300

    Exercicios 20/03

---
+++ aula_04/src/aula_04/Exercicio01.java
@@ -0,0 +1,31 @@
+package aula_04;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
+
+		int vetorInt[] = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+		int numero;
+
+		System.out.println("Digite um número: ");
+		numero = leia.nextInt();		
+
+		for (int i = 0; i < vetorInt.length; i++) {
+			if (vetorInt[i] == numero) {
+				System.out.println("O número " + numero + " está na posição " + i);
+				i = vetorInt.length;			
+			}else {
+				System.out.println("O número " + numero + " não foi encontrado");
+				i = vetorInt.length;
+			}
+			
+		}
+		
+		
+
+	}
+}
